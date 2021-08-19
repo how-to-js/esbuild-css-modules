9c8209f58235f3107f921a523b49641a05cfa4bb
Marcin Wosinek <marcin@wosinek.eu>
2021-08-19 20:59:21 +0200

    add basic application

---
+++ build.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+require("esbuild")
+  .build({
+    logLevel: "info",
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    outfile: "dist/main.js",
+  })
+  .catch(() => process.exit(1));
---
+++ dist/main.js
@@ -0,0 +1,6 @@
+(() => {
+  // src/index.js
+  var header = document.createElement("h1");
+  header.innerHTML = "Hello world";
+  document.body.appendChild(header);
+})();
